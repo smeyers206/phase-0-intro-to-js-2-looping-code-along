8fae234bb4a5400fe2cad3562a165a5382b807e8
Shane Meyers <smeyers1031@gmail.com>
2022-07-17 00:35:45 -0700

    commit tree c0fe26f9fb45e56f136defdcc0809fbdc0834224

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards() {
+    for (let i = 0; i < names.length; i++) {
+    return (`Thank you, ${names[i]}, for the wonderdul suprise gift!`)
+}
+}
+writeCards(["Guadalupe", "Ollie", "Aki"]);
+
+function countDown(integer) {
+ let i = 0;
+ while (i < 11) {
+    console.log(i++);
+ }   
+}
+countDown(10);
